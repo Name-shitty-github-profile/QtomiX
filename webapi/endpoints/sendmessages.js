1350137cd402fbe0fda96c1157b1edda0c0c5c0c
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-08-01 22:40:24 -0400

    Update sendmessages.js

--- webapi/endpoints/sendmessages.js
+++ webapi/endpoints/sendmessages.js
@@ -1,5 +1,6 @@
 const sendws = require(`${process.cwd()}/webapi/websocketmanager`);
 const whoistoken = require(`${process.cwd()}/webapi/utils/tokenidentification`);
+const getkey = require(`${process.cwd()}/webapi/utils/id_to_public_key`);
 module.exports = {
     mode: "POST",
     json: {
@@ -11,9 +12,14 @@ module.exports = {
     },
     run: function(req, res, json, header) {
         const user_id = whoistoken(header.token);
-        if (isfriend(user_id, json.recipient_id)) {
-            res.send
+        if (!isfriend(user_id, json.recipient_id)) {
+            res.status(400).send("Please add the person as friend before.");
+            return;
         }
-        sendws({"e": "NEWMESSAGE", "d": {"content": json.content, "sender_id": user_id}}, json.recipient_id);
+        if (json.content.replace(" ", '').replace("\n", '') === '') {
+            res.status(400).send("Invalid content\nPlease write something more than spaces and newlines.");
+            return;
+        }
+        sendws({"e": "NEWMESSAGE", "d": {"content": encrypt(json.content, getkey()), "sender_id": user_id}}, json.recipient_id);
     }
 };
